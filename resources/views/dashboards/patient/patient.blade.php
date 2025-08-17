3c4596dfb0fd7abee5c1635bce84fa180b31b1e1
Sayeed <sayeedibnesaif123@gmail.com>
2025-08-17 21:01:06 +0600

    Updated Patient

--- resources/views/dashboards/patient/patient.blade.php
+++ resources/views/dashboards/patient/patient.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,6 +8,7 @@
     <link rel="stylesheet" href="{{ asset('assets\Styles\patient.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 </head>
+
 <body>
     <div class="sidebar">
         <h2 class="logo"><i class="fa-solid fa-hospital"></i> HospitalMS</h2>
@@ -33,4 +35,5 @@
         </div>
     </div>
 </body>
+
 </html>
